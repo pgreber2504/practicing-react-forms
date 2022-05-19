5f1cda99a85655894226a0fc76b310492f9b5436
pgreber2504 <pgreber2504>
2022-05-19 12:14:10 +0200

    complex form validity before use hook

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,96 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+    const [nameInput, setNameInput] = useState('')
+    const [lastNameInput, setLastNameInput] = useState('')
+    const [emailInput, setEmailInput] = useState('');
+    const [nameTouched, setNameTouched] = useState(false)
+    const [lastNameTouched, setLastNameTouched] = useState(false);
+    const [emailTouched, setEmailTouched] = useState(false)
+
+    const enteredNameIsValid = nameInput.trim() !== ''
+    const enteredLastNameIsValid = lastNameInput.trim() !== ''
+    const enteredMailIsValid = emailInput.includes('@');
+
+    const invalidName = !enteredNameIsValid && nameTouched;
+    const invalidLastName = !enteredLastNameIsValid && lastNameTouched;
+    const invalidEmail = !enteredMailIsValid && emailTouched;
+
+    let formIsValid = false
+    if (enteredNameIsValid && enteredLastNameIsValid & enteredMailIsValid) formIsValid = true;
+    else formIsValid = false;
+
+    const invalidClass = 'form-control invalid'
+
+
+    const clearInputs = () => {
+        setNameInput('');
+        setLastNameInput('');
+        setEmailInput('');
+    }
+
+    const touchedAfterSubmit = (controls) => {
+        setNameTouched(controls);
+        setLastNameTouched(controls);
+        setEmailTouched(controls);
+    }
+
+
+    const nameChangeHandler = e => {
+        setNameInput(e.target.value);
+        setNameTouched(true)
+    }
+    const lastNameChangeHandler = e => {
+        setLastNameInput(e.target.value)
+        setLastNameTouched(true)
+    }
+    const emailChangeHandler = e => {
+        setEmailInput(e.target.value)
+        setEmailTouched(true);
+    }
+
+    const focusOutName = e => {
+        setNameTouched(true);
+    }
+
+    const focusOutLastName = e => {
+        setLastNameTouched(true)
+    }
+
+    const focusOutMail = e => {
+        setEmailTouched(true)
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        touchedAfterSubmit(true)
+        console.log(formIsValid);
+        if (!formIsValid) return
+        //logic
+        console.log(nameInput, lastNameInput, emailInput);
+        clearInputs();
+        touchedAfterSubmit(false)
+    }
+
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='control-group'>
-                <div className='form-control'>
+                <div className={invalidName ? invalidClass : 'form-control'}>
                     <label htmlFor='name'>First Name</label>
-                    <input type='text' id='name' />
+                    <input value={nameInput} onBlur={focusOutName} onChange={nameChangeHandler} type='text' id='name' />
                 </div>
-                <div className='form-control'>
-                    <label htmlFor='name'>Last Name</label>
-                    <input type='text' id='name' />
+                <div className={invalidLastName ? invalidClass : 'form-control'}>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input value={lastNameInput} onBlur={focusOutLastName} onChange={lastNameChangeHandler} type='text' id='lastName' />
                 </div>
             </div>
-            <div className='form-control'>
-                <label htmlFor='name'>E-Mail Address</label>
-                <input type='text' id='name' />
+            <div className={invalidEmail ? invalidClass : 'form-control'}>
+                <label htmlFor='email'>E-Mail Address</label>
+                <input value={emailInput} onBlur={focusOutMail} onChange={emailChangeHandler} type='text' id='email' />
             </div>
             <div className='form-actions'>
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     );
